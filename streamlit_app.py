262755f2d064becc498476a70555457abbf24718
nooratiffa <nur.4232401028@students.polibatam.ac.id>
2025-12-23 16:04:43 +0700

    tambah baris

--- streamlit_app.py
+++ streamlit_app.py
@@ -115,6 +115,9 @@ values = st.slider(
     label='Select a range of values',
     min_value=0, max_value=100, value=(0, 100))
 st.write('Values:', values)
-
+values = st.slider(
+    label='Select a range of values',
+    min_value=0, max_value=100, value=(0, 100))
+st.write('Values:', values)
    
  
